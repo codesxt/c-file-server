197f2fc30c296b20ac9feef64dac4fb579cb00f0
Bruno Faúndez Valenzuela <gameover_sxt_nah@hotmail.com>
2015-06-06 23:03:48 -0300

    Enviando el archivo a medias.

--- client/fileClient.c
+++ client/fileClient.c
@@ -7,37 +7,39 @@
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
-#include "../lib/visuals.h"
 #include "../lib/helpers.h"
 
+//Socket constants
 #define PORT 7070
+
+//File transfer constants
 #define MAXDATASIZE 100
-#define CHUNK_SIZE 256
+#define CHUNK_SIZE 128
 
 int main(int argc, char *argv[])
-{	
+{
 	int sockfd, numbytes;
 	char buf[MAXDATASIZE];
-	char get_file_buf[CHUNK_SIZE];
+
 	struct hostent *he;
-	struct sockaddr_in their_addr; // información de la dirección de destino
+	struct sockaddr_in their_addr;
 	if (argc != 3) {
 		fprintf(stderr,"usage: client hostname port\n");
 		exit(1);
 	}
 	char * address = argv[1];
 	int port = atoi(argv[2]);
-	
+
 	if ((he = gethostbyname(argv[1])) == NULL) {
 		perror("gethostbyname");
 		exit(1);
 	}
-	
+
 	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
 		perror("socket");
 		exit(1);
 	}
-	
+
 	their_addr.sin_family = AF_INET;
 	their_addr.sin_port = htons(port);
 	their_addr.sin_addr = *((struct in_addr *)he->h_addr);
@@ -46,45 +48,52 @@ int main(int argc, char *argv[])
 		perror("connect");
 		exit(1);
 	}
-	
+
 	printf("Conexión exitosa al servidor %s:%d.\n", address, port);
 	char command[256];
 	char *cmd;
 	char *filename;
 	while(1){
 		printf("Ingrese comandos: ");
+		memset(command, 0, sizeof(command));
 		fgets(command, sizeof(command), stdin);
 		cmd = strtok(command, " ");
-		filename = strtok(NULL, " ");
+		filename = strtok(NULL, "\n");
+		printf("%s", filename);
+		/*
+		int i = 0;
+		for(i = 0; i < strlen(filename); i++){
+			if(filename[i] == '\n')
+				filename[i] = '\0';
+		}*/
+		//filename[strlen(filename)] = '\0';
+
 		if(strcmp("traer",cmd) == 0){
 			printf("Comando: Traer\n");
 			char * request = concat(cmd, " ");
 			request = concat(request, filename);
+
 			if (send(sockfd, request, strlen(request), 0) == -1)
 				perror("send");
 			free(request);
-			FILE *fr = fopen(filename, "a");
-			bzero(get_file_buf, CHUNK_SIZE);
+
+			FILE *fw = fopen(filename, "a");
+
+			char file_buf[CHUNK_SIZE];
+			bzero(file_buf, CHUNK_SIZE);
+
 			int block_size = 0;
-			while((block_size = recv(sockfd, get_file_buf, CHUNK_SIZE, 0)) > 0){
-				int write_sz = fwrite(get_file_buf, sizeof(char), block_size, fr);
+			while((block_size = recv(sockfd, file_buf, CHUNK_SIZE, 0)) > 0){
+				int write_sz = fwrite(file_buf, sizeof(char), block_size, fw);
 				if(write_sz < block_size){
-					error("File write failed on server.\n");
+					error("File download failed.\n");
 				}
-                bzero(get_file_buf, CHUNK_SIZE);
-                if (block_size == 0 || block_size != CHUNK_SIZE) 
+                bzero(file_buf, CHUNK_SIZE);
+                if (block_size == 0 || block_size != CHUNK_SIZE)
                 {
                     break;
                 }
-
-			}
-			/*
-			if ((numbytes = recv(sockfd, buf, MAXDATASIZE-1, 0)) == -1) {
-				perror("recv");
-				exit(1);
-			}
-			buf[numbytes] = '\0';
-			printf("El servisaurio dijo: %s",buf);*/
+ 				}
 		}else if(strcmp("subir",cmd) == 0){
 			printf("Comando: Subir\n");
 		}else if(strcmp("salir\n",cmd) == 0){
@@ -93,14 +102,13 @@ int main(int argc, char *argv[])
 		}else{
 			printf("Comando inválido.\n");
 		}
-		printf("%s", filename);
-	}	
-	
+	}
+
 	if ((numbytes = recv(sockfd, buf, MAXDATASIZE-1, 0)) == -1) {
 		perror("recv");
 		exit(1);
 	}
-	
+
 	buf[numbytes] = '\0';
 	printf("Received: %s",buf);
 	close(sockfd);
--- server/fileServer.c
+++ server/fileServer.c
@@ -9,14 +9,16 @@
 #include <arpa/inet.h>
 #include <sys/wait.h>
 #include <signal.h>
-#include "../lib/visuals.h"
 #include "../lib/helpers.h"
 
+//Socket constants
 #define PORT 7070
 #define BACKLOG 5
-#define MAXDATASIZE 100
-#define COMMAND_MAX 256
-#define CHUNK_SIZE 256
+
+//File transfer constants
+#define MAXDATASIZE 100				//Buffer size in bytes for file transfer
+#define COMMAND_SIZE 256			//Buffer size in bytes for client commands
+#define CHUNK_SIZE 128				//Number of blocks read from files to send or receive
 
 void sigchld_handler(int s)
 {
@@ -26,45 +28,47 @@ void sigchld_handler(int s)
 int main(){
 	int sockfd, new_fd;
 	int buf[MAXDATASIZE];
-	char cmd_buf[1024];
+	char cmd_buf[COMMAND_SIZE];
 	struct sockaddr_in server_address;
 	struct sockaddr_in remote_address;
 	int sin_size;
 	int yes = 1;
 	int numbytes;
-	
+
 	char *cmd;
 	char *filename;
-	
+
 	struct sigaction sa;
-	
+
 	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
 		perror("socket");
 		exit(1);
 	}
-	
-	if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR,&yes,sizeof(int)) == -1) {
+
+	if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
 		perror("setsockopt");
 		exit(1);
 	}
-	
+
+	//Server socket configuration
 	server_address.sin_family = AF_INET;
 	server_address.sin_port = htons(PORT);
 	server_address.sin_addr.s_addr = INADDR_ANY;
 	memset(&(server_address.sin_zero), '\0', 8);
-	
+
 	if (bind(sockfd, (struct sockaddr *)&server_address, sizeof(struct sockaddr)) == -1) {
 		perror("bind");
 		exit(1);
 	}
-	
+
 	if (listen(sockfd, BACKLOG) == -1) {
 		perror("listen");
 		exit(1);
 	}else{
-		consoleColor(GREEN);
-		printf("Server listening on address: %s:%d%s.\n", inet_ntoa(server_address.sin_addr), ntohs(server_address.sin_port), RST);
+		printf("Server listening on address: %s:%d.\n", inet_ntoa(server_address.sin_addr), ntohs(server_address.sin_port));
 	}
+
+	//Sigaction clears dead processes
 	sa.sa_handler = sigchld_handler;
 	sigemptyset(&sa.sa_mask);
 	sa.sa_flags = SA_RESTART;
@@ -72,35 +76,42 @@ int main(){
 		perror("sigaction");
 		exit(1);
 	}
-	
+
 	while(1){
 		sin_size = sizeof(struct sockaddr_in);
 		if ((new_fd = accept(sockfd, (struct sockaddr *)&remote_address, &sin_size)) == -1) {
 			perror("accept");
 			continue;
 		}
-		consoleColor(GREEN);
-		printf("server: got connection from %s\n%s", inet_ntoa(remote_address.sin_addr), RST);
-		printf("Socket opened on port %d.\n", ntohs(remote_address.sin_port));
-		if (!fork()) { // Este es el proceso hijo
-			close(sockfd); // El hijo no necesita este descriptor
-			if ((numbytes = recv(new_fd, cmd_buf, COMMAND_MAX, 0)) == -1) {
+		printf("server: got connection from %s\n", inet_ntoa(remote_address.sin_addr));
+		printf("Socket open on port %d.\n", ntohs(remote_address.sin_port));
+		//Starts a new process to handle connection
+		if (!fork()) {
+			//Closes server listener socket
+			close(sockfd);
+
+			//Child process stays listening for commands
+			bzero(cmd_buf, sizeof(cmd_buf));
+			if ((numbytes = recv(new_fd, cmd_buf, COMMAND_SIZE, 0)) == -1) {
 				perror("recv");
 				exit(1);
 			}
-			cmd_buf[strlen(cmd_buf)-1] = '\0';
-			//printf("Received: %s", cmd_buf);
+
+			//Command and filename are separated in two char *
 			cmd = strtok(cmd_buf, " ");
-			filename = strtok(NULL, " ");
+			filename = strtok(NULL, "\n");
+
 			if(strcmp("traer",cmd) == 0){
 				printf("Client requested update of file %s.\n", filename);
+
 				FILE *fs = fopen(filename, "r");
 				if(fs == NULL)
 				{
 				    printf("ERROR: File %s not found.\n", filename);
 				    exit(1);
 				}
-				char file_buf[CHUNK_SIZE]; 
+
+				char file_buf[CHUNK_SIZE];
 				int block_size = 0;
 				while((block_size = fread(file_buf, sizeof(char), CHUNK_SIZE, fs)) > 0){
 					if(send(new_fd, file_buf, block_size, 0) < 0){
