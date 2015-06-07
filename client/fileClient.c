9cd34451b1ce0e2ecec72bc6e3b0bc2159ec3edf
Bruno Faúndez Valenzuela <gameover_sxt_nah@hotmail.com>
2015-06-07 15:55:29 -0300

    A little license fix.

--- client/fileClient.c
+++ client/fileClient.c
@@ -13,7 +13,7 @@
  * fileServer.c is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation, either version 3 of the License, or (at your option) any later version.
- * tum_ardrone is distributed in the hope that it will be useful, but WITHOUT ANY
+ * fileClient.c is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
  * PARTICULAR PURPOSE.  See the GNU General Public License for more details.
  *
